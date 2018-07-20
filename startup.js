57d5de9f730454f1e3c92e239ba600eacefa1b62
SoNiceElijah <Superil2q@yandex.ru>
2018-07-20 20:56:05 +0300

    LongPoll start

--- startup.js
+++ startup.js
@@ -2,10 +2,12 @@
 const port = 1234;
 const mongoUrl = "mongodb://localhost:27017/";
 
+var longPollPort = 1235;
+
 //Системные подключения
 const express = require('express'),
     bodyParser = require('body-parser'),
-    http = require('http'), 
+    http = require('http'),
     jsonParser = bodyParser.json(),
     mongoClient = require("mongodb");
 
@@ -15,18 +17,19 @@ const login = require('./App/Routes/login.js');
 const sendMessage = require('./App/Routes/sendMessage.js');
 const getMessages = require('./App/Routes/getMessages.js');
 const authMiddleware = require('./App/Routes/AuthMiddleware.js');
-const LongPoll = require('./App/Routes/LongPoll.js');
 
 //Приложение
 const app = express();
+//Лонг пул
+const longPoll = express();
 
 //Вспомогательные стили
 app.get("/App/Styles/style.css", function (request, response) {
-    response.sendfile("./App/Styles/style.css",function(){response.end();});
+    response.sendfile("./App/Styles/style.css", function () { response.end(); });
 });
 
 app.use(jsonParser);
-app.use(function (req, res, next) {    
+app.use(function (req, res, next) {
     //request ip middlaware
     console.log("New request from: " + res.connection.remoteAddress);
     next();
@@ -35,35 +38,60 @@ app.use(function (req, res, next) {
 //Сайт
 //Отдельно, но тоже не будет работать без базы данных
 app.get("/", function (request, response) {
-    response.sendfile("./App/Pages/debug.html",function(){response.end();});   
+    response.sendfile("./App/Pages/debug.html", function () { response.end(); });
 });
 
 
 //Сервер
-mongoClient.connect(mongoUrl, { useNewUrlParser: true }, function(err, client) {
-	if(err) {
-		console.log('DataBase conntect error');
-	} else {
+mongoClient.connect(mongoUrl, { useNewUrlParser: true }, function (err, client) {
+    if (err) {
+        console.log('DataBase conntect error');
+    } else {
 
-        app.use(function(req, res, next) {
-			req.db = client.db("ezWebChat");
-			next();
-        });        
-        
+        app.use(function (req, res, next) {
+            req.db = client.db("ezWebChat");
+            next();
+        });
+
         app.post("/login", login.login);
         app.post("/register", register.register);
-        
+
         //Auth
         app.use(authMiddleware.Auth);
-        
+
         app.post("/getMessagesHistory", getMessages.getMessagesHistory);
         app.post("/sendMessage", sendMessage.sendMessage);
-        
-        //app.post("/getNewMessages", LongPoll.getNewMesseges);
-        
-        http.createServer(app).listen(port, function() {
-        console.log("listen on port: ", port);
+
+        //Выделяем новый сервер
+        app.post("/getLongPollServer", function (req, res) {
+
+            longPoll.listen(longPollPort, function () {
+                console.log("LongPoll on: ", longPollPort);
+            });
+
+            res.send(JSON.stringify(
+                {
+                    Success: true,
+                    Content: null,
+                    LongPollPort: longPollPort
+                }
+            ));
+
+            longPollPort++;
+        });
+
+        longPoll.use(function (req, res, next) {
+            req.db = client.db("ezWebChat");
+            next();
+        });
+
+        longPoll.use(authMiddleware.Auth);
+        longPoll.post("/GetNewMessages", function (req, res) {
+
+        });
+
+        http.createServer(app).listen(port, function () {
+            console.log("listen on port: ", port);
         });
     }
 });
-
